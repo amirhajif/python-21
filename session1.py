50d2d72a2eea2616f567726b6aba31308bdfdb25
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-08-16 11:42:30 +0330

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,110 @@
+# print("hello")
+# salam in jalase aval class ast
+# class 7 nafar hastim
+
+"""
+adjklasdl;aksdl;aksl;da
+sdaskldal;skdlaks'dkasd
+asdk;asdl;kasdlas;das
+ak;sdasldasdl;as
+"""
+
+# variable
+# studentname = "amirhossein"
+# score = 20
+
+# print(studentname)
+
+# user_name1 = "amir"
+# user_name2 = "reza"
+
+# username = "amir hossein "
+
+# username = "amir"
+
+# student_name="reza"
+
+# bestStudentScore = 19
+
+
+# name = "amir"
+
+# name = input("please enter name: ")
+# print(name)
+
+# print("amirhossein",end=" ")
+# print("hajitabar")
+# print("programming Teacher")
+ 
+# name = "amirhossein hajitabar"
+# print(name)
+# print(len(name))
+# print(name[3])
+# print(name[-1])
+# print(name[0:4])
+# print(name[:4])
+# print(name[4:])
+# print(name[4:21:2])
+# print(name[::-1])
+
+# name = "amir"
+# print(name.upper())
+
+# city = "BABOL"
+# print(city.lower())
+
+# message = "im from iran"
+# print(message.title())
+
+# firstName = "amirhossein"
+# print(firstName.replace("s","m"))
+
+# products = "apple,xiaomi,samsung"
+# splitedProduct = products.split(",")
+# print(splitedProduct)
+
+# firstName = input("please enter firstname: ")
+# lastName = input("please enter lastname: ")
+
+# print(f"welcome mr {firstName} {lastName}")
+# print("welcome mr {} {}".format(firstName,lastName))
+
+
+# print("amirhossein\nhajitabar\tprogramming Teacher")
+
+# int,float
+# score = 20 #int
+# height=1.80 #float
+
+
+# a=5
+# b=2
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a**b)
+# print(a%b)
+
+
+# a=int(input("please enter a: "))
+# b=input("please enter b: ")
+
+# print(a*6)
+
+# name = "amir"
+# lastname = "hajitabar"
+# print(name*4)
+# print(name+lastname)
+
+# name,family,score = "amir","hajitabar",20
+
+# name = "amir"
+# family = "hajitabar"
+# score=20
+
+
+money = 1_000_000_000
+print(money)
