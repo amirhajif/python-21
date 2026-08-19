936838af38568318a56f2f113cc85c9ddca81d7e
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-08-19 10:48:55 +0330

    Create Excersice1.py

---
+++ Excersice1.py
@@ -0,0 +1,23 @@
+# E1
+# date = "2025-07-23" #--->"2025/07/23"
+# # print(date.replace("-","/"))
+
+# newDate = date.replace("-","/")
+# print(newDate)
+
+
+# E2
+# a=int(input("please enter a: "))
+# b=int(input("please enter b: "))
+# c=int(input("please enter c: "))
+# x=int(input("please enter x: "))
+
+# y=(a*x**2)+(b*x)+c
+
+# print(y)
+
+# 1f = 1.8c+32 
+
+# c=int(input("plaese enter temp: "))
+# f= 1.8*c+32
+# print(f)
