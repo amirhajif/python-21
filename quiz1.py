cc22fea7d13d52571bb6c25de49c57f121337c71
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-08-19 09:49:21 +0330

    Create quiz1.py

---
+++ quiz1.py
@@ -0,0 +1,7 @@
+# name = input("please enter name: ")
+
+# pl1 = input("please enter programming language 1: ")
+# pl2 = input("please enter programming language 2: ")
+# pl3 = input("please enter programming language 3: ")
+
+# print(f"my name is {name}:\n\t1-{pl1}\n\t2-{pl2}\n\t3-{pl3}")
