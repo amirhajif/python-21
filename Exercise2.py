11f0914708c289fa1fe968a19e7b6b3ed1f19d34
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-08-23 11:11:43 +0330

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,82 @@
+# E1
+# names = ["mohammad", "ali", "ahmad", "reza", "keyvan"]
+# a = max(names,key=len)
+# print(a)
+
+# E2
+# a = [2,5,7]
+# b = [3,6,4]
+
+# if sum(a) > sum(b):
+#     a.reverse()
+#     print(a)
+# else:
+#     b.reverse()
+#     print(b)
+    
+    
+# E3
+# numbers = [1,2,1]
+
+# if numbers == numbers[:-1]:
+#     print ("barabar")
+# else:
+#     print("na barabar")
+# print(numbers.reverse())
+# if numbers == numbers.reverse():
+#     print ("barabar")
+# else:
+#     print("na barabar")
+
+
+# E4
+# scores = [20,14,15,6,8]
+
+# m_x = max(scores)
+# m_n = min(scores)
+# scores.remove(m_x)
+# scores.remove(m_n)
+
+# print(sum(scores)/len(scores))
+
+# E5
+# sound = ['a','e','i','o','u']
+# Letter = input("Letter:").lowe()
+
+# if Letter in sound:
+#     print("true")
+# else:
+#     print("false")    
+
+# letter = input("please enter your letter: ").lower()
+# if letter in "a,e,i,o,u":
+#     print("vowel")
+# else:
+#     print("not vowel")    
+
+
+# E6
+# allowed_names=["amir","ali","ahmad","reza"]
+# public_passwords=["guest","temp"]
+
+# name=input("pleas enter name:")
+# card=input("you have student cart(yes/no):")
+# password=input("enter the password:")
+
+# if name in allowed_names and card=="yes" and password in public_passwords :
+#     print("entry is permitted")
+# else:
+#     print("unauthorized entry")
+
+# E7
+# major = input("please enter major:")
+# average = float(input("please enter average:"))
+
+# if(major=="Medicine" or major=="Engineering")and average>18:
+#     print("specialized")
+# elif major=="Literature" and average>16:
+#     print("cultural")
+# else:
+#     print("free")
+
+# E8
